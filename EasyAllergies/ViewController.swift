821e841fcde4bfacefdde53ed30e475218d05985
Michael Klein <kleinmu@bc.edu>
2021-05-08 20:40:54 -0400

    Initial Commit

---
+++ EasyAllergies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyAllergies
+//
+//  Created by Michael Klein on 5/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
